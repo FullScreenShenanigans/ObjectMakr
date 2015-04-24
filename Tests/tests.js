d11db7c5a47784a5572aa6a2240335f2427c9acd
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-04-23 23:21:49 -0400

    Added unit tests, through constructor
    More to be added soon.

--- Tests/tests.js
+++ Tests/tests.js
@@ -1,5 +1,44 @@
-describe("constructor", function () {
-    it("works", function () {
+var ObjectMaker,
+    inheritance = {
+        "Shape": {
+            "Rectangle": {
+                "Square": {
+                    "UnitSquare": {}
+                }
+            }
+        }
+    },
+    properties = {
+        "Rectangle": {
+            "numSides": 4,
+            "equalSides": false,
+            "getArea": function () {
+                return this.width * this.height
+            }
+        },
+        "Square": {
+            "equalSides": true
+        }
+    },
+    indexMap = ["width", "height"];
 
+describe("constructor", function () {
+    it("throws an error when not given inheritance", function () {
+        chai.expect(function () {
+            new ObjectMakr({});
+        }).to.throw("No inheritance mapping given to ObjectMakr.");
+    });
+
+    it("initializes inheritance", function () {
+        chai.expect(new ObjectMakr({
+            "inheritance": inheritance
+        }).getInheritance()).to.be.deep.equal(inheritance);
+    });
+
+    it("initializes properties", function () {
+        chai.expect(new ObjectMakr({
+            "inheritance": inheritance,
+            "properties": properties
+        }).getProperties()).to.be.deep.equal(properties);
     });
 });
