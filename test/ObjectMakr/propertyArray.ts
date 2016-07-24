72e9d5f175809e1de45e922c62d463b11a8f8c13
Josh Goldberg <josh@fullscreenmario.com>
2016-07-24 13:05:07 -0700

    Brought in gulp-shenanigans 0.3.1

--- test/ObjectMakr/propertyArray.ts
+++ test/ObjectMakr/propertyArray.ts
@@ -1,8 +1,8 @@
 /// <reference path="../../node_modules/@types/chai/index.d.ts" />
 /// <reference path="../../node_modules/@types/mocha/index.d.ts" />
 /// <reference path="../../lib/ObjectMakr.d.ts" />
-/// <reference path="../utilities/MochaLoader.ts" />
-/// <reference path="../utilities/mocks.ts" />
+/// <reference path="../utils/MochaLoader.ts" />
+/// <reference path="../utils/mocks.ts" />
 
 mochaLoader.addTest("creates an object with the mapped properties", (): void => {
     // Arrange
